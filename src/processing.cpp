f7bc373926a62bf300852e9fcaf1da0fcd3fe1c3
Taylor Foxhall <lxmachina@gmail.com>
2015-12-13 23:11:07 -0500

    that's no good

--- src/processing.cpp
+++ src/processing.cpp
@@ -31,12 +31,61 @@ void plateBounds(const cv::Mat& src, cv::Mat& dst) {
   dst = img;
 }
 
-bool hasTextRatio(const cv::Rect& bound) {
-  
+bool hasTextRatio(const std::vector<cv::Point>& contour, cv::Size imgSize) {
+  cv::Rect box = cv::boundingRect(contour);
+  double ratio = (double)box.width / box.height;
+
+  if (ratio < 0.1 || ratio > 10) {
+    return false;
+  }
+  int boxArea = box.width * box.height;
+  int imgArea = imgSize.width * imgSize.height;
+  return boxArea >= 15 && boxArea <= imgArea / 5;
 }
 
-bool keep(const std::vector<cv::Point>& contour) {
-  return hasTextRatio(contour) && isConnected(contours);
+// bool isConnected(const std::vector<cv::Point>& contour) {
+//   contour[0]
+// }
+
+bool keep(const std::vector<cv::Point>& contour, cv::Size imgSize) {
+  return hasTextRatio(contour, imgSize);
+}
+
+int countChildren(const std::vector<std::vector<cv::Point> >& contours,
+                  const std::vector<cv::Vec4i>& hier,
+                  int index,
+                  cv::Size imgSize) {
+  if (hier[index][2] < 0) {
+    // no children
+    return 0;
+  }
+
+  // Do I have a child?
+  int child = hier[index][2];
+  int count = 0;
+  if (keep(contours[child], imgSize)) {
+    count = 1;
+  }
+
+  // Count my children's children
+  count += countChildren(contours, hier, child, imgSize);
+
+  // Count siblings
+  // Make sure it's the children's siblings
+  for (int next = hier[child][0]; next > 0; next = hier[next][0]) {
+    if (keep(contours[next], imgSize)) {
+      count++;
+    }
+    count += countChildren(contours, hier, next, imgSize);
+  }
+  for (int prev = hier[child][0]; prev > 0; prev = hier[prev][0]) {
+    if (keep(contours[prev], imgSize)) {
+      count++;
+    }
+    count += countChildren(contours, hier, prev, imgSize);
+  }
+
+  return count;
 }
 
 void textBinary(const cv::Mat& src, cv::Mat& dst) {
@@ -54,9 +103,23 @@ void textBinary(const cv::Mat& src, cv::Mat& dst) {
   std::vector<cv::Vec4i> hierarchy;
   cv::findContours(img, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_NONE);
 
+  std::vector<cv::Rect> keptRegions;
   for (int i = 0; i < contours.size(); i++) {
     cv::Rect bound = cv::boundingRect(contours[i]);
 
+    // find the parent contour, if there exists one
+    int parent;
+    for (parent = hierarchy[i][3];
+         parent > 0 && keep(contours[parent], img.size());
+         parent = hierarchy[parent][3]);
+
+    int numChildren = countChildren(contours, hierarchy, i, img.size());
+    if (keep(contours[i], img.size()) &&
+        !(parent > 0 && numChildren <= 2) &&
+        numChildren <= 2) {
+      keptRegions.push_back(bound);
+      cv::rectangle(img, bound, cv::Scalar(255, 0, 0));
+    }
   }
 
   dst = img;
